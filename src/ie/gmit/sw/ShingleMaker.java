813412dae811d33fcbc91df67a38d44679e61b15
PedroHOMota <phmota@hotmail.com>
2018-01-02 22:25:52 +0000

    Implemented method to create and hash the shingles

--- src/ie/gmit/sw/ShingleMaker.java
+++ src/ie/gmit/sw/ShingleMaker.java
@@ -5,15 +5,30 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class ShingleMaker 
 {
-	public static ArrayList<String> MakeShingles(ArrayList<String> txtFile) throws IOException
+	public static HashSet<Integer> MakeShingles(ArrayList<String> txtFile) throws IOException
 	{
 		ArrayList<String> parsedText = new ArrayList<String>();
+		int nRows=SHINGLEMAXLIST;
+		if(parsedText.size()<SHINGLEMAXLIST)
+			nRows=parsedText.size();
 		
+		int i=0;
+		HashSet<Integer> hashShingle=new HashSet<>();
+		while(i<nRows)
+		{
+			String[] aux=parsedText.get(i).split("[ ]");
+			String shingleAux="";
+			shingleAux = aux[i]+aux[i+1]+aux[i+2];
+			hashShingle.add(shingleAux.hashCode());
+			i+=3;
+		}
 		
-		return parsedText;
+		return hashShingle;
+		
 		
 	}
 }
